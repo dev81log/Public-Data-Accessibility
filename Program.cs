e7d562473f495087a20b3dce5b278d028ae7d573
dev81log <dev81log@gmail.com>
2022-08-24 17:34:49 -0300

    Add conexão SqlConnection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+var connection = new SqlConnection(connectionString);
