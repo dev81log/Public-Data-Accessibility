271c5a3f84fbed75ef0de70b2e79794da60e6c9f
dev81log <dev81log@gmail.com>
2022-08-25 12:02:08 -0300

    Finalizado a construção da category e conectando e inserindo dados no DB

--- Program.cs
+++ Program.cs
@@ -11,17 +11,48 @@ namespace OpenDataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazonaws.com";
+            category.Summary = "AWS Cloud";
+            category.Order = 8;
+            category.Description = "Amazon Web Services (AWS) is a web service that makes it easy to deploy";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO [Category] 
+                            VALUES(
+                                @Id,
+                                @Title,
+                                @Url,
+                                @Summary,
+                                @Order,
+                                @Description,
+                                @Featured
+                            )";
 
             using (var connection = new SqlConnection(connectionString))
             {
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var category in categories)
+                var rows = connection.Execute(insertSql, new
                 {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"{rows} rows inserted");
+
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
-
-
         }
+
+
     }
 }
