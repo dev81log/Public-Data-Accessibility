506bb1e8adb2b877e9334a193102ed79c6f3928a
dev81log <dev81log@gmail.com>
2022-08-25 10:23:55 -0300

    Iniciando Dapper

--- Program.cs
+++ Program.cs
@@ -11,25 +11,10 @@ namespace OpenDataAcess
 
             using (var connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Conectado");
-                connection.Open();
 
-
-                using (var command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                    }
-                }
             }
 
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
