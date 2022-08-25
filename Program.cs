b5c3b78de416383b1790e33273452a05e34b459f
dev81log <dev81log@gmail.com>
2022-08-25 16:23:25 -0300

    Finalizado metodos para list, create e update

--- Program.cs
+++ Program.cs
@@ -11,6 +11,26 @@ namespace OpenDataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                UpdateCategory(connection);
+                ListCategories(connection);
+                //CreateCategory(connection);
+            }
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        static void CreateCategory(SqlConnection connection)
+        {
             var category = new Category();
             category.Id = Guid.NewGuid();
             category.Title = "Amazon AWS";
@@ -19,7 +39,6 @@ namespace OpenDataAccess
             category.Order = 8;
             category.Description = "Amazon Web Services (AWS) is a web service that makes it easy to deploy";
             category.Featured = false;
-
             var insertSql = @"INSERT INTO [Category] 
                             VALUES(
                                 @Id,
@@ -30,29 +49,32 @@ namespace OpenDataAccess
                                 @Description,
                                 @Featured
                             )";
-
-            using (var connection = new SqlConnection(connectionString))
+            var rows = connection.Execute(insertSql, new
             {
-                var rows = connection.Execute(insertSql, new
-                {
-                    category.Id,
-                    category.Title,
-                    category.Url,
-                    category.Summary,
-                    category.Order,
-                    category.Description,
-                    category.Featured
-                });
-                Console.WriteLine($"{rows} rows inserted");
-
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var item in categories)
-                {
-                    Console.WriteLine($"{item.Id} - {item.Title}");
-                }
-            }
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"{rows} rows inserted");
         }
 
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = @"UPDATE [Category]
+                                SET [Title] = @Title
+                                WHERE [Id] = @Id";
 
+            var rows = connection.Execute(updateQuery, new
+            {
+                Id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                Title = "Front End 2021"
+            });
+
+            Console.WriteLine($"{rows} rows updated");
+        }
     }
 }
