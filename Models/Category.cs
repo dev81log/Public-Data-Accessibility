8d76ab1adcb7a5fde72181f818fdb9ca112f9a93
dev81log <dev81log@gmail.com>
2022-08-25 12:03:04 -0300

    add do file ao git

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,6 +6,11 @@ namespace OpenDataAccess.Models
     {
         public Guid Id { get; set; }
         public string? Title { get; set; }
+        public string? Url { get; set; }
+        public string? Summary { get; set; }
+        public int Order { get; set; }
+        public string? Description { get; set; }
+        public bool Featured { get; set; }
 
     }
 }
