fc581e69fafd12be967c19c9c5ab7b25de34cc63
dev81log <dev81log@gmail.com>
2023-09-23 18:27:47 -0300

    Insert ExecuteProcedure

--- Program.cs
+++ Program.cs
@@ -1,22 +1,24 @@
-﻿using System;
-using Dapper;
+﻿using Dapper;
 using Microsoft.Data.SqlClient;
 using OpenDataAccess.Models;
+using System.Data;
 
-namespace OpenDataAccess
+namespace BaltaDataAccess
 {
     class Program
     {
         static void Main(string[] args)
         {
-            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
-
+            const string connectionString = "Server=localhost,1433;Database=dapperDBCurse;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
             using (var connection = new SqlConnection(connectionString))
             {
-                UpdateCategory(connection);
+                // CreateManyCategory(connection);
+                // UpdateCategory(connection);
+                // DeleteCategory(connection);
                 ListCategories(connection);
-                //CreateCategory(connection);
+                // GetCategory(connection);
+
             }
         }
 
@@ -29,27 +31,54 @@ namespace OpenDataAccess
             }
         }
 
-        static void CreateCategory(SqlConnection connection)
+        static void GetCategory(SqlConnection connection)
+        {
+            var category = connection
+                .QueryFirstOrDefault<Category>(
+                    "SELECT TOP 1 [Id], [Title] FROM [Category] WHERE [Id]=@id",
+                    new
+                    {
+                        id = "af3407aa-11ae-4621-a2ef-2028b85507c4"
+                    });
+            Console.WriteLine($"{category.Id} - {category.Title}");
+
+        }
+
+        static void CreateManyCategory(SqlConnection connection)
         {
             var category = new Category();
             category.Id = Guid.NewGuid();
-            category.Title = "Amazon AWS";
-            category.Url = "amazonaws.com";
-            category.Summary = "AWS Cloud";
-            category.Order = 8;
-            category.Description = "Amazon Web Services (AWS) is a web service that makes it easy to deploy";
+            category.Title = "Azure";
+            category.Url = "azure";
+            category.Description = "Categoria destinada a serviços do Azure";
+            category.Order = 7;
+            category.Summary = "Azure";
             category.Featured = false;
-            var insertSql = @"INSERT INTO [Category] 
-                            VALUES(
-                                @Id,
-                                @Title,
-                                @Url,
-                                @Summary,
-                                @Order,
-                                @Description,
-                                @Featured
-                            )";
-            var rows = connection.Execute(insertSql, new
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Google Cloud";
+            category2.Url = "google";
+            category2.Description = "Categoria destinada a serviços do Google Cloud";
+            category2.Order = 9;
+            category2.Summary = "Google Cloud";
+            category2.Featured = false;
+
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            var rows = connection.Execute(insertSql, new[]
+            {
+                new
             {
                 category.Id,
                 category.Title,
@@ -58,23 +87,54 @@ namespace OpenDataAccess
                 category.Order,
                 category.Description,
                 category.Featured
+            },
+                new
+            {
+                category2.Id,
+                category2.Title,
+                category2.Url,
+                category2.Summary,
+                category2.Order,
+                category2.Description,
+                category2.Featured
+            },
             });
-            Console.WriteLine($"{rows} rows inserted");
+
+            Console.WriteLine($"{rows} linhas inseridas");
         }
+
 
         static void UpdateCategory(SqlConnection connection)
         {
-            var updateQuery = @"UPDATE [Category]
-                                SET [Title] = @Title
-                                WHERE [Id] = @Id";
-
+            var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
             var rows = connection.Execute(updateQuery, new
             {
-                Id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
-                Title = "Front End 2021"
+                id = new Guid("e7a697b0-4706-458d-aa82-91d6874a453f"),
+                title = "Frontend 2021"
             });
 
-            Console.WriteLine($"{rows} rows updated");
+            Console.WriteLine($"{rows} registros atualizadas");
         }
+
+        static void DeleteCategory(SqlConnection connection)
+        {
+            var deleteQuery = "DELETE [Category] WHERE [Id]=@id";
+            var rows = connection.Execute(deleteQuery, new
+            {
+                id = new Guid("e7a697b0-4706-458d-aa82-91d6874a453f"),
+            });
+
+            Console.WriteLine($"{rows} registros excluídos");
+        }
+
+        static void ExecuteProcedure(SqlConnection connection)
+        {
+            var procedure = "[spDeleteStudent]";
+            var pars = new { StudentId = "" };
+            var affectedRows = connection.Execute(procedure, pars, commandType: CommandType.StoredProcedure);
+
+            Console.WriteLine($"{affectedRows} registros excluídos");
+        }
+
     }
 }
